31c15774e26866535804b8a52a71c56fc25467c1
Abhishek D C <Abhishek D C@LAPTOP-GUFIAA4N>
2024-06-25 18:50:39 +0530

    first commit

---
+++ b96 core java/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,13 @@
+//program to demonstrate the encapsulation//
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialNum;//Data members//
+	String name;
+	int age;
+	
+	void show() 
+	{
+		System.out.println("serialnumber:"+serialNum+"\n Name:"+name +"\n Age:"+age);
+	}
+}
---
+++ b96 core java/src/com/tnsif/encapsulation/Encapsulationdemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulationdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation ob= new Encapsulation();//object creation
+		ob.name="abhi";//calling the variable
+		ob.age=23;
+		ob.serialNum=22;
+		ob.show();
+
+	}
+
+}
