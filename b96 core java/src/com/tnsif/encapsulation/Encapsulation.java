fe07b941d9115f7c2d098c656901a4acc9d902a7
Abhishek D C <Abhishek D C@LAPTOP-GUFIAA4N>
2024-06-25 19:35:45 +0530

    commit

--- b96 core java/src/com/tnsif/encapsulation/Encapsulation.java
+++ b96 core java/src/com/tnsif/encapsulation/Encapsulation.java
@@ -2,12 +2,38 @@
 package com.tnsif.encapsulation;
 
 public class Encapsulation {
-	int serialNum;//Data members//
-	String name;
-	int age;
-	
-	void show() 
-	{
-		System.out.println("serialnumber:"+serialNum+"\n Name:"+name +"\n Age:"+age);
+	private int serialNum;//Data members//
+	private String name;
+	private int age;
+	//by using the getters and setters we can access the private data members//
+	public int getSerialNum() {
+		return serialNum;
 	}
+// to display string represetation of the string
+	@Override
+	public String toString() {
+		return "Encapsulation [serialNum=" + serialNum + ", name=" + name + ", age=" + age + "]";
+	}
+
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	
 }
--- b96 core java/src/com/tnsif/encapsulation/Encapsulationdemo.java
+++ b96 core java/src/com/tnsif/encapsulation/Encapsulationdemo.java
@@ -5,10 +5,14 @@ public class Encapsulationdemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Encapsulation ob= new Encapsulation();//object creation
-		ob.name="abhi";//calling the variable
-		ob.age=23;
-		ob.serialNum=22;
-		ob.show();
+		//ob.name="abhi";//calling the variable
+		//ob.age=23;
+		//ob.serialNum=22;
+		
+		ob.setAge(22);
+		ob.setName("abhi");
+		ob.setSerialNum(2);
+		System.out.println(ob);
 
 	}
 
