c3939df69beef8f8e851fc2171b0d454e27e260f
Abhishek D C <Abhishek D C@LAPTOP-GUFIAA4N>
2024-09-02 21:37:31 +0530

    commit

---
+++ b96 core java/src/com/tnsif/polymorphism/PolyDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.polymorphism;
+
+public class PolyDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Point pone=new Point();
+		System.out.println(pone);
+		
+		Point ptwo=new Point(2.3f , 3.4f);
+		System.out.println(ptwo);
+
+	}
+
+}
